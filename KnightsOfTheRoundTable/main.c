7a6bc243675947cbd495ae6698b8c55aeb41234f
Akhil Verghese <akhilcherian@gmail.com>
2013-05-08 23:44:48 +0530

    Solution to Spoj Problem http://www.spoj.com/problems/KNIGHTSR/

--- KnightsOfTheRoundTable/main.c
+++ KnightsOfTheRoundTable/main.c
@@ -6,13 +6,27 @@
 //  Copyright (c) 2013 Akhil Verghese. All rights reserved.
 //
 
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(int argc, const char * argv[])
+int main()
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int i,n=1;
+    double ans=0;
+    while(n!=0)
+    {
+        scanf("%d\n",&n);
+        if(n==1)
+            printf("With 1 competitor, a Jedi Knight will be replaced approximately 0.00 times.\n");
+        else if(n>1)
+        {
+            for (i=2;i<=n;i++)
+            {
+                ans+=(1.0000/i);
+            }
+            printf("With %d competitors, a Jedi Knight will be replaced approximately %0.2lf times.\n",n,ans);
+            ans=0;
+        }
+    }
 }
 
